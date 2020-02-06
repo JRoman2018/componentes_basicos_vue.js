17b05182fe071ba0c27b5e4f032da308940ee41f
jose roman <el-1ero@outlook.com>
2020-02-06 19:46:41 -0400

    componentes

---
+++ componentes/app.js
@@ -0,0 +1,22 @@
+Vue.component('titulo', {
+    template: `
+    <div>
+        <h1>{{titulo}}</h1>
+        <h3>fasdasds</h3>
+    </div>
+    `,
+
+    data() {
+        return {
+            titulo: 'Aprendiendo Componentes Basicos en Vue.js',
+        }
+    }
+});
+
+// const app = new Vue({
+//     el: '#app',
+//     data: {
+
+//     },
+
+// });
